37b515393b23f5e5615ed93c8dcac0d4e8bf36e7
Kevin Hörner <kevinjoa@vmd4622.contabo.net>
2014-04-11 16:48:01 +0200

    carbon-ify

---
+++ carbon.mk
@@ -0,0 +1,27 @@
+# name
+PRODUCT_RELEASE_NAME := Optimus G
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 768
+
+# Inherit some common CARBON stuff.
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/e975/e975.mk)
+
+# languages
+PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := e975
+PRODUCT_NAME := carbon_e975
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-E975
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=geehrc_open_eu BUILD_FINGERPRINT=lge/geehrc_open_eu/geehrc:4.1.2/JZO54K/E97510b.1360741472:user/release-keys PRIVATE_BUILD_DESC="geehrc_open_eu-user 4.1.2 JZO54K E97510b.1360741472 release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
